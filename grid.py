aa0fcd88ae685d2704a9405c34a0bf888be9f9a0
browning <bwbrowning@gmail.com>
2013-06-02 22:30:41 -0500

    added a content security policy to stop people from loading images and scripts from other servers

--- grid.py
+++ grid.py
@@ -1,10 +1,12 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, make_response
 app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return render_template('grid.html')
+    response = make_response(render_template('grid.html'))
+    response.headers['Content-Security-Policy'] =  "default-src: 'none'; script-src: 'self';"
+    return response
 
 if __name__ == "__main__":
     app.run(debug=True)
