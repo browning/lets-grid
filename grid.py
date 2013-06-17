2311d37f07d7598eb276a3b3f5ad8d2048fd385a
browning <bwbrowning@gmail.com>
2013-06-16 22:17:12 -0500

    provide routing for tutorial links

--- grid.py
+++ grid.py
@@ -19,23 +19,46 @@ def create():
 
 @app.route("/save_grid", methods=["POST"])
 def save_grid():
-	js_code = request.form['codearea']
-	app_id = grid_data.save_grid(js_code)
-	flash('Copy the URL in the url bar to share your creation with friends')
-	return redirect(url_for('show_grid', grid_id=app_id))
+    js_code = request.form['codearea']
+    app_id = grid_data.save_grid(js_code)
+    flash('Copy the URL in the url bar to share your creation with friends')
+    return redirect(url_for('show_grid', grid_id=app_id))
 
 @app.route("/grid/<grid_id>")
 def show_grid(grid_id):
-	js_data = grid_data.load_grid(grid_id)
-	response = make_response(render_template('grid.html', usrjs=js_data))
-	response.headers['Content-Security-Policy'] =  "default-src: 'none'; script-src: 'self';"
-	return response
+    js_data = grid_data.load_grid(grid_id)
+    response = make_response(render_template('grid.html', usrjs=js_data))
+    response.headers['Content-Security-Policy'] =  "default-src: 'none'; script-src: 'self';"
+    return response
 
 @app.route("/tutorials")
 def tutorials_list():
     response = make_response(render_template('tutorials.html'))
     response.headers['Content-Security-Policy'] =  "default-src: 'none'; script-src: 'self';"
     return response
+
+@app.route("/tutorials/<tutorial_name>")
+def show_tutorial(tutorial_name):
+    if tutorial_name == "basics":
+        response = make_response(render_template('basics_tutorial.html'))
+    elif tutorial_name == "variables":
+        response = make_response(render_template('variables_tutorial.html'))
+    elif tutorial_name == "functions":
+        response = make_response(render_template('functions_tutorial.html'))
+    elif tutorial_name == "if-statements":
+        response = make_response(render_template('if_tutorial.html'))
+    elif tutorial_name == "loops":
+        response = make_response(render_template('loops_tutorial.html'))
+    elif tutorial_name == "objects":
+        response = make_response(render_template('objects_tutorial.html'))
+    elif tutorial_name == "advcanced":
+        response = make_response(render_template('advanced_tutorial.html'))
+    elif tutorial_name == "resources":
+        response = make_response(render_template('resources_tutorial.html'))
+    response.headers['Content-Security-Policy'] =  "default-src: 'none'; script-src: 'self';"
+    return response
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
